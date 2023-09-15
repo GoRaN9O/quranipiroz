889f9fa8860259f4a9188a3277d7d12cb174b4fc
kurdikeyboard <3160786+GoRaN909@users.noreply.github.com>
2023-09-15 04:32:10 +0300

    ....

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import Vue from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import i18n from '@/plugins/i18n';
+import M from 'materialize-css/dist/js/materialize';
+import 'materialize-css/dist/css/materialize.min.css';
+import VuePlyr from 'vue-plyr';
+import 'vue-plyr/dist/vue-plyr.css';
+import '@/assets/scss/main.scss';
 
-createApp(App).mount('#app')
+M.AutoInit();
+
+Vue.use(VuePlyr, {
+  plyr: {
+    fullscreen: { enabled: false },
+    // autoplay: true
+  },
+  emit: ['ended']
+});
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+}).$mount('#app')
